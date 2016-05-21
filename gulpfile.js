4dd17ac08fc45d38e0c5b62747bd1d189cda9311
Rodrigo Brito <rig.10@hotmail.com>
2016-05-21 20:32:52 -0300

    Mudando estilo de saida do css para EXPANDED

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var sass = require('gulp-sass');
 
 gulp.task('styles', function() {
     gulp.src('sass/style.scss')
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
         .pipe(gulp.dest('./css'))
 });
 
